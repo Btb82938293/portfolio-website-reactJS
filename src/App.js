d2640cacdc77eceab5ef814f95cd9a2ff7988b2e
Дмитрий Засыпкин <dmitrijzasypkin@MacBook-Air-som-tillhor-Dmitrij.local>
2023-08-27 10:03:55 +0300

    Projects page completed

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
           <Route path="/projects" element={<Projects />} />
           <Route path="/about" element={<About />} />
         </Routes>
-        <Footer />
+        {/* <Footer /> */}
       </div>
     </Router>
   );
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,7 +1,66 @@
+import img from "../../Assets/project-img.jpg"
+
 export default function Projects() {
     return (
-        <div>
-            <h1>Please get acquainted with my projects</h1>
+        <div className="projects-main">
+            <h1>My recent works</h1>
+            <p className="colored projects-text">Here are some projects I have worked on recently</p>
+            <div className="projects-container">
+                <div className="project">
+                    <img className="project-img" src={img}></img>
+                    <p className="project-name">Tenzies</p>
+                    <p className="project-description">Lorem ipsum dolor sit amet, 
+                    consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+                    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis 
+                    nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+                    consequat. </p>
+                </div>
+                <div className="project">
+                    <img className="project-img" src={img}></img>
+                    <p className="project-name">Tenzies</p>
+                    <p className="project-description">Lorem ipsum dolor sit amet, 
+                    consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+                    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis 
+                    nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+                    consequat. </p>
+                </div>
+                <div className="project">
+                    <img className="project-img" src={img}></img>
+                    <p className="project-name">Tenzies</p>
+                    <p className="project-description">Lorem ipsum dolor sit amet, 
+                    consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+                    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis 
+                    nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+                    consequat. </p>
+                </div>
+                <div className="project">
+                    <img className="project-img" src={img}></img>
+                    <p className="project-name">Tenzies</p>
+                    <p className="project-description">Lorem ipsum dolor sit amet, 
+                    consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+                    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis 
+                    nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+                    consequat. </p>
+                </div>
+                <div className="project">
+                    <img className="project-img" src={img}></img>
+                    <p className="project-name">Tenzies</p>
+                    <p className="project-description">Lorem ipsum dolor sit amet, 
+                    consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+                    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis 
+                    nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+                    consequat. </p>
+                </div>
+                <div className="project">
+                    <img className="project-img" src={img}></img>
+                    <p className="project-name">Tenzies</p>
+                    <p className="project-description">Lorem ipsum dolor sit amet, 
+                    consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+                    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis 
+                    nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+                    consequat. </p>
+                </div>
+            </div>
         </div>
     )
 }
